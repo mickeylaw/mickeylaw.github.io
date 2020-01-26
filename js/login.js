33b7c6757c6dcca4ca9c1c10d5e3b5c1cbf0cf2f
Mickey <56874813+wiiinng@users.noreply.github.com>
2020-01-26 16:58:21 +0000

    Homepage

--- js/login.js
+++ js/login.js
@@ -1,1 +1,17 @@
-login.js
+document.onkeypress = enter;
+function enter(e) {
+    if (e.keyCode == 13) {
+        checkPswd();
+    }
+}
+
+function checkPswd() {
+    var confirmPassword = "1234";
+    var password = document.getElementById("pswd").value;
+    if (password == confirmPassword) {
+         window.open("5indeskset.html");
+    }
+    else{
+        alert("Invalid password, please try again.");
+    }
+}
