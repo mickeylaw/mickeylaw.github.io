efb52348e528dcfc79a0e61dfa8bc24e1bd9f942
Mickey <56874813+wiiinng@users.noreply.github.com>
2020-12-03 22:16:22 +0800

    Update scrollsection.js

--- js/scrollsection.js
+++ js/scrollsection.js
@@ -5,24 +5,26 @@ let data = document.querySelector("#vz-data");
 let objectives = document.querySelector("#vz-objectives");
 let ideation = document.querySelector("#vz-ideation");
 
-window.addEventListener("scroll",()=>{
-    var windo = window.pageYOffset;
-  if(context.offsetTop <= windo && solution.offsetTop > windo){
-      console.log("Context");
-  }
-  else if(solution.offsetTop <= windo && process.offsetTop > windo){
-      console.log("Solution");
-  }
-  else if(process.offsetTop <= windo && data.offsetTop > windo){
-      console.log("Process");
-  }
-  else if(data.offsetTop <= windo && objectives.offsetTop > windo){
-      console.log("Data");
-  }
-  else if(objectives.offsetTop <= windo && ideation.offsetTop > windo){
-      console.log("Objectives");
-  }
-  else {
-    console.log("Home");
-}
+document.addEventListener('DOMContentLoaded', function() {
+    window.addEventListener("scroll",()=>{
+        var windo = window.pageYOffset;
+    if(context.offsetTop <= windo && solution.offsetTop > windo){
+        console.log("Context");
+    }
+    else if(solution.offsetTop <= windo && process.offsetTop > windo){
+        console.log("Solution");
+    }
+    else if(process.offsetTop <= windo && data.offsetTop > windo){
+        console.log("Process");
+    }
+    else if(data.offsetTop <= windo && objectives.offsetTop > windo){
+        console.log("Data");
+    }
+    else if(objectives.offsetTop <= windo && ideation.offsetTop > windo){
+        console.log("Objectives");
+    }
+    else {
+        console.log("Home");
+    }
+    })
 });
